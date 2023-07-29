3287418469517255350ecb95f9dcb81b4f40aeac
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-07-29 09:50:27 +0530

    Add files via upload

---
+++ d2Prog1.java
@@ -0,0 +1,19 @@
+
+class rc
+{
+	public static void main(String args[])
+	{
+
+		System.out.println("Ritam Chakrabortty");
+		main("Techno International New Town");
+
+
+	}
+	public static void main(String args)
+	{
+
+
+		System.out.println(args);
+
+	}
+}	
