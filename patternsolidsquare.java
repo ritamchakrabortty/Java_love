ed18ea82e430fefb5e4116f4d93efe3f37286ad8
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-10-03 09:44:41 +0530

    Add files via upload

---
+++ patternsolidsquare.java
@@ -0,0 +1,17 @@
+//pattern solid rectangle
+import java.util.*;
+class pat
+{
+  public static void main(String rc[])
+  {
+    for(int i=1;i<=10;i++)
+    {
+      for(int j=1;j<=10;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+}
