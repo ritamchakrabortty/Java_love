610703bb7fb80f684248558cc3f0d5d1c2635b08
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-10-06 17:43:28 +0530

    Add files via upload

---
+++ patternhalfpyramid.java
@@ -0,0 +1,21 @@
+//patternhalfpyramid
+import java.util.*;
+class pat
+{
+  public static void main(String args[])
+  {
+    for(int i=1;i<=50;i++)
+    {
+      for(int j=1;j<=i;j++)
+      
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    
+    }
+  }
+}
+
+
+
