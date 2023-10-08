53e47ebea7e2e65b69aca3dba0700f084fe8d055
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-10-08 13:43:03 +0530

    Add files via upload

---
+++ invbetedHalfPyramidWithNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class pat
+{
+  public static void main(String aa[])
+  {
+    for(int i=1;i<=50;i++)
+    {
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print(j+" ");
+      }
+      System.out.println();
+    }
+  }
+  
+}
+
