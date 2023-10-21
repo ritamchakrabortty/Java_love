fafe3aee9c8e22b30d330553db76c6a7a446fc5f
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-10-21 20:24:06 +0530

    Add files via upload

---
+++ PatternButterfly.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class pat
+{
+  public static void main(String aa[])
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter maximum bound: ");
+    int n=sc.nextInt();
+    //upper half
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      int s=2*(n-i);
+      for(int j=1;j<=s;j++)
+      {
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    //lower
+    for(int i=n;i>=1;i--)
+    {
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      int s=2*(n-i);
+      for(int j=1;j<=s;j++)
+      {
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+  }
+}
+
