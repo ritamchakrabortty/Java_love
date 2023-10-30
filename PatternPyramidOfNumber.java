b15fd7cfa791f28e7e4652ded9018cd5b192c64c
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-10-30 23:17:59 +0530

    Add files via upload

---
+++ PatternPyramidOfNumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class pat
+{
+	public static void main(String dc[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter maximum bound: ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		
+		}
+		
+	
+	}
+
+}
