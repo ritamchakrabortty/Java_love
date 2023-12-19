d8005f8e42208193c21056a08f819cdae9bbe1a0
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-12-19 20:21:12 +0530

    Update hasnext.java

--- hasnext.java
+++ hasnext.java
@@ -29,7 +29,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-class Solution {
+public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
