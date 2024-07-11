8575e3587f2e17d948bc22e0bf902569049491e8
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2024-07-11 23:05:47 +0530

    Add files via upload

---
+++ Java_DSA/1Darray.java
@@ -0,0 +1,19 @@
+class Demo
+{
+    public static void main(String[] args)
+    {
+        int nums[]={3,7,2,4};
+        int nums1[]= new int[4];
+        nums [1]=6;
+        //System.out.println(nums[1]);
+        nums1[0]=4;
+        nums1[1]=8;
+        nums1[2]=3;
+        nums1[3]=9;
+        for (int i=0; i<4; i++)
+        {
+           System.out.println(nums1[i]);
+        }
+    }
+
+}
